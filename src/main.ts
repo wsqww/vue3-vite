db976b80257d32ec8437b10a6f568fdc99667e9a
wsq <wangsq0926@163.com>
2022-06-21 16:36:35 +0800

    create repository

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import store from '@/store';
+import router from '@/router';
+// 创建vue实例
+const app = createApp(App);
+// 挂载 pinia
+app.use(store);
+app.use(router);
+// 挂载实例
+app.mount('#app');
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
